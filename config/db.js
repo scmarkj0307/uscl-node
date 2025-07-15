01de9420c51b12785ef7e5c6f17f9f9d9c9f0360
Markjarnaiz <Sc.markj0307@gmail.com>
2025-07-15 21:51:24 +0800

    DATABASE: add option reminder for port and instance

--- config/db.js
+++ config/db.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 const sql = require('mssql');
 
 const config = {
-  user: process.env.DB_USER,
+  user: process.env.DB_USER,    
   password: process.env.DB_PASSWORD,
   server: process.env.DB_SERVER,
   database: process.env.DB_NAME,
   options: {
-    port: parseInt(process.env.DB_PORT, 10),
+    instanceName: process.env.DB_INSTANCE,  //use instance or port depends on your sql server connection
     encrypt: false,
     trustServerCertificate: true
   }
