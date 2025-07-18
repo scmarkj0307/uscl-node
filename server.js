cbda8e1f5c045b29535894b92dd58cfcdea64f20
Markjarnaiz <Sc.markj0307@gmail.com>
2025-07-18 14:31:41 +0800

    ARCHITECTURE: update the server so render can run the code

--- server.js
+++ server.js
@@ -18,5 +18,8 @@ app.use('/api/clients', clientsRouter);
 app.use('/api/transaction-history', transactionHistoryRouter);
 
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
+
