a22f0164b7017761b59cfb5841577894d5377e31
Markjarnaiz <Sc.markj0307@gmail.com>
2025-07-18 14:06:08 +0800

    ARCHITECTURE: cleanup and add starting command

--- config/db.js
+++ config/db.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.PG_HOST,         // e.g., db.abcd.supabase.co
-  port: 5432,                        // default PostgreSQL port
-  user: process.env.PG_USER,         // from Supabase
-  password: process.env.PG_PASSWORD, // from Supabase
-  database: process.env.PG_DATABASE, // from Supabase
+  host: process.env.PG_HOST,       
+  port: 5432,                        
+  user: process.env.PG_USER,        
+  password: process.env.PG_PASSWORD, 
+  database: process.env.PG_DATABASE, 
   ssl: {
-    rejectUnauthorized: false        // needed for Supabase
+    rejectUnauthorized: false       
   }
 });
 
